fb4cb1d54f8ba9969d8b77c9750a677dcdf96102
ndt0123 <43299711+ndt0123@users.noreply.github.com>
2019-04-22 11:50:11 +0700

    Thêm đường dẫn của hình ảnh vào cơ sở dữ liệu

--- controller/uploadNewRoom.php
+++ controller/uploadNewRoom.php
@@ -3,53 +3,51 @@
 ?>
 
 <?php
-	if(isset($_POST["DangTin"])){
+	if(isset($_POST['title']) && isset($_POST['KieuPhong']) && isset($_POST['VeSinh']) && isset($_POST['price']) && isset($_POST['area']) && isset($_POST['electricPrice']) && isset($_POST['waterPrice']) && isset($_POST['doiTuong']) && isset($_POST['tienIch']) && isset($_POST['quanHuyen']) && isset($_POST['xaPhuong']) && isset($_POST['diaChi']) && isset($_POST['tenChuTro']) && isset($_POST['sdt']) && isset($_POST['moTa']) && isset($_SESSION['user_name'])) {
+
 		//kết nối đến CSDL
 		include('connectToDatabase.php');
+
+		$last_id = 0; //Lưu id của phòng trọ vừa đăng
 
 		//Câu lệnh sql insert giá trị vào bảng dia_chi_phong_tro
 		$sql_insert_dia_chi_phong_tro = 'INSERT INTO dia_chi_phong_tro(DiaChi, XaPhuong, QuanHuyen, TenChuTro, Sdt) VALUE("' .$_POST['diaChi']. '", "' .$_POST['xaPhuong']. '", "' .$_POST['quanHuyen']. '", "' .$_POST['tenChuTro']. '", "' .$_POST['sdt']. '")';
 
-		mysqli_query($conn, $sql_insert_dia_chi_phong_tro);
+		if(mysqli_query($conn, $sql_insert_dia_chi_phong_tro)) {
+			$last_id = mysqli_insert_id($conn);
 
-		// Lấy id phòng trọ vừa thêm vào
+			//Câu lệnh sql insert giá trị vào bảng gia_phong_tro
+			$sql_insert_gia_phong_tro = 'INSERT INTO gia_phong_tro(IDPhongTro, user_name, TieuDe, KieuPhong, KieuVeSinh, GiaChoThue, DienTich, GiaDien, GiaNuoc, DoiTuong, TienIch, MoTa, ThoiGianDang) VALUE("' .$last_id. '", "' .$_SESSION['user_name']. '", "' .$_POST['title']. '", "' .$_POST['KieuPhong']. '", "' .$_POST['VeSinh']. '", "' .$_POST['price']. '", "' .$_POST['area']. '", "' .$_POST['electricPrice']. '", "' .$_POST['waterPrice']. '", "' .$_POST['doiTuong']. '", "' .$_POST['tienIch']. '", "' .$_POST['moTa']. '", "' .date('Y-m-d H:i:s'). '")';
 
-		$idPhongTro = mysqli_insert_id($conn);
+			mysqli_query($conn, $sql_insert_gia_phong_tro);
 
-		//Câu lệnh sql insert giá trị vào bảng gia_phong_tro
+			//Tải hình ảnh lên csdl
+			$sql_insert_imags = 'INSERT INTO hinh_anh_phong_tro(IDPhongTro, DuongDan) VALUE'; //Câu lệnh sql thêm hình ảnh
 
-		$sql_insert_gia_phong_tro = 'INSERT INTO gia_phong_tro(IDPhongTro, user_name, TieuDe, KieuPhong, KieuVeSinh, GiaChoThue, DienTich, GiaDien, GiaNuoc, DoiTuong, TienIch, MoTa, ThoiGianDang) VALUE("' .$idPhongTro. '", "' .$_SESSION['user_name']. '", "' .$_POST['title']. '", "' .$_POST['KieuPhong']. '", "' .$_POST['VeSinh']. '", "' .$_POST['price']. '", "' .$_POST['area']. '", "' .$_POST['electricPrice']. '", "' .$_POST['waterPrice']. '", "' .$_POST['doiTuong']. '", "' .$_POST['tienIch']. '", "' .$_POST['moTa']. '", "' .date('Y-m-d H:i:s'). '")';
-		
-		mysqli_query($conn, $sql_insert_gia_phong_tro);
+			$target_dir = "uploads/"; //folder lưu hình ảnh
 
-		//Tải hình ảnh lên csdl
-		$sql_insert_imags = 'INSERT INTO hinh_anh_phong_tro(IDPhongTro, DuongDan) VALUE'; //Câu lệnh sql thêm hình ảnh
+			$num_of_imgs = count($_FILES['fileToUpload']['name']); //số hình ảnh upload lên
 
-		$target_dir = "uploads/"; //folder lưu hình ảnh
+			if($num_of_imgs>0) {
+				$target_file = array(); //mảng lưu đường dẫn của từng hình ảnh
+				for($i=0; $i<$num_of_imgs; $i++) {
+					$target_file[$i] = $target_dir . basename($_FILES["fileToUpload"]["name"][$i]); //lấy đường dẫn của từng hình ảnh
 
-		$num_of_imgs = count($_FILES['fileToUpload']['name']); //số hình ảnh upload lên
+				}
 
-		$target_file = array(); //mảng lưu đường dẫn của từng hình ảnh
-		for($i=0; $i<$num_of_imgs; $i++) {
-			$target_file[$i] = $target_dir . basename($_FILES["fileToUpload"]["name"][$i]); //lấy đường dẫn của từng hình ảnh
+				for($i=0; $i<$num_of_imgs; $i++) {
+			    	if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"][$i], $target_file[$i])) {
+			            if($i==$num_of_imgs-1) {
+			                $sql_insert_imags = $sql_insert_imags. '("' .$last_id. '", "' .$target_file[$i]. '")';
+			            } else {
+			                $sql_insert_imags = $sql_insert_imags. '("' .$last_id. '", "' .$target_file[$i]. '"),';
+			            }
+			        }
+			    }
+			    mysqli_query($conn, $sql_insert_imags);
+			}
 
 		}
-
-		for($i=0; $i<$num_of_imgs; $i++) {
-	    	if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"][$i], $target_file[$i])) {
-	            if($i==$num_of_imgs-1) {
-	                $sql_insert_imags = $sql_insert_imags. '("' .$idPhongTro. '", "' .$target_file[$i]. '")';
-	            } else {
-	                $sql_insert_imags = $sql_insert_imags. '("' .$idPhongTro. '", "' .$target_file[$i]. '"),';
-	            }
-	        } else {
-	            echo "Sorry, there was an error uploading your file. <br/>";
-	        }
-	    }
-	    mysqli_query($conn, $sql_insert_imags);
-
-	    header("Location: http://localhost/TroTotHN/index.php");
-		
 	}
 
 
